cc53f3cb488b9c060c507c3384081700660136b2
Luís Carlos <77598201+Lucal22@users.noreply.github.com>
2022-07-16 02:32:06 -0300

    adding nextjs plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:tailwindcss/recommended',
+    'plugin:@next/next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
